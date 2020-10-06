bbd4479bad42300864a0b7d187d90041f22b6b24
marius-perera <marius.perera@xero.com>
2020-10-06 17:37:04 +1100

    Create simple express server and add a new API end point

--- src/controllers/alfresco.controller.ts
+++ src/controllers/alfresco.controller.ts
@@ -1,12 +1,17 @@
-// import * as Express from 'express';
+import express, { Request, Response } from 'express';
 import { APIError } from '../helpers/errorHandlers/APIError';
 // import { parseString } from 'xml2js';
+import status from 'http-status';
 import { alfrescoConfig } from '../config/config';
 
 export function getAlfrescoAccessTokens(): boolean {
-  const error = new APIError('Test Error', 500);
+  const error = new APIError('Test Error', status.INTERNAL_SERVER_ERROR);
   if (alfrescoConfig.host) {
     return true;
   }
   return !error;
+}
+
+export function get(req: Request, res: Response): express.Response<any> {
+  return res.send('Express + TypeScript Server');
 }
